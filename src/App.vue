e191a1c01d0947530dcf2ef69f9cf3e393ee9721
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2025-05-01 20:06:49 -0500

    Add `TimerView`

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import TimerView from './views/TimerView.vue';
+
 </script>
 
 <template>
-
   <main>
+    <TimerView></TimerView>
   </main>
 </template>
 
---
+++ src/views/TimerView.vue
@@ -0,0 +1,10 @@
+<!-- Main View With Buttons -->
+<script setup lang="ts">
+</script>
+
+<template>
+	<p>this is where the buttons go</p>
+</template>
+
+<style scoped>
+</style>
