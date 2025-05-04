74751a215f7fab6e91faf2aa93fe13570e2cab83
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2025-05-04 13:53:51 -0500

    Create `preferencesStore`

--- src/stores/preferencesStore.ts
+++ src/stores/preferencesStore.ts
@@ -1,4 +1,11 @@
+import type { Sink } from '@/models/sink';
 import { ref } from 'vue';
 
 // by default, only show seconds in dev
-export const prefShowSeconds = ref<boolean>(import.meta.env.NODE_ENV === 'development');
+export const prefShowSeconds = ref<boolean>(import.meta.env.DEV);
+
+export const prefSinks = ref<Sink[]>(
+  ['AFK', 'Work', 'Play', 'Meetings', 'Coding', 'Ricing'].map((s) => ({
+    name: s,
+  })),
+);
--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -1,18 +1,14 @@
 <!-- Main View With Buttons -->
 <script setup lang="ts">
 import { ref } from 'vue';
-import { type Sink } from '../models/sink.ts';
 import SinkButtons from '@/components/SinkButtons.vue';
 import { useIntervalFn } from '@vueuse/core';
 import { recordTick, totalTicks } from '@/stores/trackingStore.ts';
 import StopwatchDisplay from '@/components/StopwatchDisplay.vue';
 import HistoryDisplay from '@/components/HistoryDisplay.vue';
+import { prefSinks } from '@/stores/preferencesStore';
 
-const sinks: Sink[] = ['afk', 'work', 'play', 'meetings', 'coding', 'ricing'].map((s) => ({
-  name: s,
-}));
-
-const focusedSink = ref(sinks[0].name);
+const focusedSink = ref(prefSinks.value[0].name);
 
 const onTick = () => {
   recordTick(focusedSink.value);
@@ -23,7 +19,7 @@ const { pause, resume, isActive } = useIntervalFn(onTick, 1000);
 </script>
 
 <template>
-  <SinkButtons :sinks v-model="focusedSink" />
+  <SinkButtons :sinks="prefSinks" v-model="focusedSink" />
   <StopwatchDisplay :n-ticks="totalTicks" />
   <HistoryDisplay :max-entries="1"></HistoryDisplay>
 </template>
