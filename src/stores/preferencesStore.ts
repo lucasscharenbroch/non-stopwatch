9a34c9be968227c7797c5d644834857e48242fc6
Lucas Scharenbroch <lucasscharenbroch@gmail.com>
2025-05-04 18:19:05 -0500

    Save config in local storage

--- src/stores/preferencesStore.ts
+++ src/stores/preferencesStore.ts
@@ -1,11 +1,12 @@
 import type { Sink } from '@/models/sink';
 import { DEFAULT_COLORS } from '@/utils/color';
-import { ref } from 'vue';
+import { useLocalStorage } from '@vueuse/core';
 
 // by default, only show seconds in dev
-export const prefShowSeconds = ref<boolean>(import.meta.env.DEV);
+export const prefShowSeconds = useLocalStorage<boolean>('showSeconds', import.meta.env.DEV);
 
-export const prefSinks = ref<Sink[]>(
+export const prefSinks = useLocalStorage<Sink[]>(
+  'sinks',
   ['AFK', 'Work', 'Play', 'Meetings', 'Coding', 'Ricing'].map((s, i) => ({
     name: s,
     colorHex: DEFAULT_COLORS[i % DEFAULT_COLORS.length],
